fe708623fd759a8e94f3a9d7106bc8ef6850d24d
bASILgIL <basil.gil@polymtl.ca>
2020-09-15 22:50:16 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,7 +4,7 @@
 
 def is_even_len(string: str) -> bool:
     if int(len(str)/2):
-        print()
+        print(f"Le nombre de caractère dans la chaine {chaine} est pair")
     else:
         pass
     return
