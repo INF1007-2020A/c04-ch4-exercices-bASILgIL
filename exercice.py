035c5b296d1e1eea37283d4e706ef2957a3abb81
bASILgIL <basil.gil@polymtl.ca>
2020-09-15 23:49:28 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,7 +18,7 @@ def remove_third_char(string: str) -> str:
 def replace_char(string: str, old_char: str, new_char: str) -> str:
         for i in range(len(string)):
             if string[i] == old_char:
-                string = string[:i] 
+                string = string[:i] + 
     return 
 
 
