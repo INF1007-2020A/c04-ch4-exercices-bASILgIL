2e864a2b7937a1af89c8a52f82122230750dbc1d
bASILgIL <basil.gil@polymtl.ca>
2020-09-15 23:47:12 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 def get_number_of_char(string: str, char: str) -> int:
     
-    return string.count()
+    return string.count(char)
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
