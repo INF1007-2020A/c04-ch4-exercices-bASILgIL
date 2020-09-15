10436fa3c3166dced1fc98283d77daecbf93275f
bASILgIL <basil.gil@polymtl.ca>
2020-09-15 23:56:37 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -28,7 +28,7 @@ def get_number_of_char(string: str, char: str) -> int:
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
-    return string.count(word)
+    return sentence.count(word)
 
 
 def main() -> None:
